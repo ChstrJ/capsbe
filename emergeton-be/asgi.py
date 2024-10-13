02043e409fa8681aad351838a1487887a7638d2c
Chester <89991461+ChstrJ@users.noreply.github.com>
2024-10-14 07:48:15 +0800

    rename app

--- emergeton-be/asgi.py
+++ emergeton-be/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for capsbe project.
+ASGI config for emergeton-be project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'capsbe.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emergeton-be.settings')
 
 application = get_asgi_application()
--- emergeton-be/wsgi.py
+++ emergeton-be/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for capsbe project.
+WSGI config for emergeton-be project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'capsbe.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emergeton-be.settings')
 
 application = get_wsgi_application()

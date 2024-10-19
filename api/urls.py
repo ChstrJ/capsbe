44cb4716dd250b9ae05950327109deee805ff4b2
ChstrJ <chestervjazmin@gmail.com>
2024-10-19 12:03:20 +0800

    add comments

--- api/urls.py
+++ api/urls.py
@@ -31,29 +31,29 @@ from .views.alert_view import (
 )
 
 urlpatterns = [
-    path('residents', GetResidentsView.as_view(), name='get-residents'),
-    path('residents/paginate', PaginateResidentsView.as_view(), name='paginate-residents'),
-    path('residents/<uuid:pk>', FindResidentView.as_view(), name='find-resident'),
-    path('residents/create', CreateResidentView.as_view(), name='create-resident'),
-    path('residents/delete/<uuid:pk>', DeleteResidentView.as_view(), name='delete-resident'),
-    path('residents/update/<uuid:pk>', UpdateResidentView.as_view(), name='update-resident'),
+    path('residents', GetResidentsView.as_view(), name='get-residents'),  # get
+    path('residents/paginate', PaginateResidentsView.as_view(), name='paginate-residents'),  # get
+    path('residents/<uuid:pk>', FindResidentView.as_view(), name='find-resident'), # get/id
+    path('residents/create', CreateResidentView.as_view(), name='create-resident'), # post
+    path('residents/delete/<uuid:pk>', DeleteResidentView.as_view(), name='delete-resident'), # delete
+    path('residents/update/<uuid:pk>', UpdateResidentView.as_view(), name='update-resident'), # put/patch
     
-    path('auth/login', LoginView.as_view(), name='admin-login'),
-    path('auth/register/admin', AdminRegisterView.as_view(), name='admin-register'),
-    path('auth/register/resident', ResidentRegisterView.as_view(), name='resident-register'),
+    path('auth/login', LoginView.as_view(), name='admin-login'), # post
+    path('auth/register/admin', AdminRegisterView.as_view(), name='admin-register'), # post
+    path('auth/register/resident', ResidentRegisterView.as_view(), name='resident-register'), # post
     
     path('departments', GetDepartmentsView.as_view(), name='get-departments'),
-    path('departments/generate', GenerateDepartmentsView.as_view(), name='generate-departments'),
-    path('departments/create', CreateDepartmentView.as_view(), name='create-department'),
-    path('departments/delete/uuid:pk', DeleteDepartmentView.as_view(), name='delete-department'),
-    path('departments/update/uuid:pk', UpdateDepartmentView.as_view(), name='update-department'),
-    path('departments/available', GetAvailableCountView.as_view(), name='get-available'),
+    path('departments/generate', GenerateDepartmentsView.as_view(), name='generate-departments'), # post
+    path('departments/create', CreateDepartmentView.as_view(), name='create-department'), # post
+    path('departments/delete/uuid:pk', DeleteDepartmentView.as_view(), name='delete-department'), # delete
+    path('departments/update/uuid:pk', UpdateDepartmentView.as_view(), name='update-department'), # put/patch
+    path('departments/available', GetAvailableCountView.as_view(), name='get-available'), # get
     
-    path('alerts', ListAlertsView.as_view(), name='list-alert'),
-    path('send/alert', CreateAlertView.as_view(), name='send-alert'),
-    path('send-sms/fire', SendSmsView.as_view(), name='send-sms'),
-    path('send-sms/police', SendSmsView.as_view(), name='send-sms'),
-    path('send-sms/medical', SendSmsView.as_view(), name='send-sms'),
+    path('alerts', ListAlertsView.as_view(), name='list-alert'), # get
+    path('send/alert', CreateAlertView.as_view(), name='send-alert'), # post
+    path('send-sms/fire', SendSmsView.as_view(), name='send-sms'), # post
+    path('send-sms/police', SendSmsView.as_view(), name='send-sms'), # post
+    path('send-sms/medical', SendSmsView.as_view(), name='send-sms'), # post
     
-    path('send-email', SendEmailView.as_view(), name='send-sms'),
+    path('send-email', SendEmailView.as_view(), name='send-sms'), # post
 ]

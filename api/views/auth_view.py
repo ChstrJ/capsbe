8e64a4089fee5d5dca189e47b628c05f191d3ed4
ChstrJ <chestervjazmin@gmail.com>
2024-10-19 12:42:23 +0800

    nice

--- api/views/auth_view.py
+++ api/views/auth_view.py
@@ -1,5 +1,4 @@
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework import status
 from ..serializers.user_serializer import UserSerializer, ResidentSerializer, AdminSerializer, LoginSerializer
--- api/views/resident_view.py
+++ api/views/resident_view.py
@@ -1,9 +1,9 @@
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework import status
-from ..serializers.resident_serializer import ResidentSerializer
+from ..serializers.user_serializer import ResidentSerializer, UserSerializer
 from ..messages import *
 from ..helpers import response
-from ..models import Resident
+from ..models import Resident, User
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 
@@ -26,31 +26,55 @@ class PaginateResidentsView(APIView):
 class FindResidentView(APIView):
     permission_classes = [AllowAny]
     def get(self, request, pk):
+        
         try:
-            resident = Resident.objects.get(pk=pk)
-            serializer = ResidentSerializer(resident)
-            return response(serializer.data, SUCCESS, status.HTTP_200_OK)
+            resident = Resident.objects.get(user_id=pk)
         except Resident.DoesNotExist:
-            return response(serializer.data, NOT_FOUND, status.HTTP_404_NOT_FOUND)
-
-class CreateResidentView(APIView):
-    permission_classes = [AllowAny]
-    def post(self, request):
-        serializer = ResidentSerializer(data=request.data)
-        if serializer.is_valid():
-            try:
-                serializer.save()
-                return response(serializer.data, CREATED, status.HTTP_201_CREATED)
-            except Exception as e:
-                return response(e, SERVER_ERROR, status.HTTP_500_INTERNAL_SERVER_ERROR)
-        else:
-            return response(serializer.errors, BAD_REQUEST, status.HTTP_400_BAD_REQUEST)
+            return response(False, NOT_FOUND, status.HTTP_404_NOT_FOUND)
+        
+        serializer = ResidentSerializer(resident)
+        
+        resident_data = serializer.data
+        
+        data = resident_data.get('user')
+        
+        response_data = {
+                "id": data['id'],
+                "first_name": data['first_name'],
+                "last_name": data['last_name'],
+                "email": data['email'],
+                "username":data['username'],
+                "user_type":  data['username'],
+                "created_at": data['created_at'],
+                "updated_at": data['updated_at'],
+                "contact_number":resident_data['contact_number'],
+                "address":resident_data['address'],
+                "landmark":resident_data['landmark'],
+                "latitude":resident_data['latitude'],
+                "longitude":resident_data['longitude'],
+        }
+        
+        
+        return response(response_data, SUCCESS, status.HTTP_200_OK)    
+           
+# class CreateResidentView(APIView):
+#     permission_classes = [AllowAny]
+#     def post(self, request):
+#         serializer = ResidentSerializer(data=request.data)
+#         if serializer.is_valid():
+#             try:
+#                 serializer.save()
+#                 return response(serializer.data, CREATED, status.HTTP_201_CREATED)
+#             except Exception as e:
+#                 return response(e, SERVER_ERROR, status.HTTP_500_INTERNAL_SERVER_ERROR)
+#         else:
+#             return response(serializer.errors, BAD_REQUEST, status.HTTP_400_BAD_REQUEST)
 
 class DeleteResidentView(APIView):
     permission_classes = [AllowAny]
     def delete(self, request, pk):
         try:
-            resident = Resident.objects.get(pk=pk)
+            resident = Resident.objects.get(user_id=pk)
             resident.delete()
             return response(True, DELETED, status.HTTP_200_OK)
         except Resident.DoesNotExist:

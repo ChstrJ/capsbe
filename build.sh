f331d93ed59272f242953bd34ab0207628ac85e1
ChstrJ <chestervjazmin@gmail.com>
2024-10-30 01:04:39 +0000

    test

--- build.sh
+++ build.sh
@@ -8,5 +8,19 @@ pip install -r requirements.txt
 # Convert static asset files
 python manage.py collectstatic --no-input
 
+if [f "db.sqlite3"]; then
+	rm db.sqlite3
+	echo "deleted"
+else 
+	echo "pass"
+fi
+
+
 # Apply any outstanding database migrations
+python manage.py makemigrations
 python manage.py migrate
+
+# Generate test account
+python manage.py account
+python manage.py generate
+

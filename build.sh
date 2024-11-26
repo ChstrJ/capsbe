055c62bf7cbf2bae77614567e633e05216780e60
ChstrJ <chestervjazmin@gmail.com>
2024-11-26 17:43:14 +0800

    s

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 # python manage.py makemigrations
-python manage.py migrate
+# python manage.py migrate
 
 # Generate test account
 # python manage.py account
